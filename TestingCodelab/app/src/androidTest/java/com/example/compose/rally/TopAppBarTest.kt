34c40892ba692cedbe1dfbb1eabf4aed4d972d9f
Álvaro Radajczyk <40389952+AlvaroRadajczykProjects@users.noreply.github.com>
2023-10-17 17:58:42 +0200

    step_4

--- TestingCodelab/app/src/androidTest/java/com/example/compose/rally/TopAppBarTest.kt
+++ TestingCodelab/app/src/androidTest/java/com/example/compose/rally/TopAppBarTest.kt
@@ -1,10 +1,16 @@
 package com.example.compose.rally
 
+import androidx.compose.ui.test.assertHasClickAction
 import androidx.compose.ui.test.assertIsSelectable
 import androidx.compose.ui.test.assertIsSelected
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithContentDescription
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.onRoot
+import androidx.compose.ui.test.printToLog
 import com.example.compose.rally.ui.components.RallyTopAppBar
+import org.junit.After
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 
@@ -14,8 +20,8 @@ class TopAppBarTest {
     val composeTestRule = createComposeRule()
     //val composeTestRule = createAndroidComposeRule(RallyActivity::class.java)
 
-    @Test
-    fun myTest() {
+    @Before
+    fun init(){
         val allScreens = RallyScreen.entries //RallyScreen.values().toList()
 
         composeTestRule.setContent {
@@ -25,13 +31,31 @@ class TopAppBarTest {
                 currentScreen = RallyScreen.Accounts
             )
         }
+    }
 
+    @After
+    fun tearDown() {
+
+    }
+
+    @Test
+    fun myTest() {
         //Thread.sleep(5000)
-
         composeTestRule
             .onNodeWithContentDescription(RallyScreen.Accounts.name)
             .assertIsSelected()
+    }
 
+    @Test
+    fun accountsIsUpperCase(){
+
+        composeTestRule.onRoot().printToLog("currentLabelExists")
+
+        composeTestRule
+            //.onNodeWithContentDescription("Accounts_BUTTON")
+            //.onNodeWithText(RallyScreen.Accounts.name.uppercase())
+            .onNodeWithContentDescription(RallyScreen.Accounts.name)
+            .assertExists()
     }
 
 
