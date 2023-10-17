a8484bf117410a2f75e44634d67f83f12fb05afb
Álvaro Radajczyk <40389952+AlvaroRadajczykProjects@users.noreply.github.com>
2023-10-17 18:09:14 +0200

    step_5

--- TestingCodelab/app/src/androidTest/java/com/example/compose/rally/TopAppBarTest.kt
+++ TestingCodelab/app/src/androidTest/java/com/example/compose/rally/TopAppBarTest.kt
@@ -3,6 +3,9 @@ package com.example.compose.rally
 import androidx.compose.ui.test.assertHasClickAction
 import androidx.compose.ui.test.assertIsSelectable
 import androidx.compose.ui.test.assertIsSelected
+import androidx.compose.ui.test.hasContentDescription
+import androidx.compose.ui.test.hasParent
+import androidx.compose.ui.test.hasText
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.compose.ui.test.onNodeWithText
@@ -47,7 +50,7 @@ class TopAppBarTest {
     }
 
     @Test
-    fun accountsIsUpperCase(){
+    fun accountsButtonExists(){
 
         composeTestRule.onRoot().printToLog("currentLabelExists")
 
@@ -58,5 +61,19 @@ class TopAppBarTest {
             .assertExists()
     }
 
+
+    @Test
+    fun accountsIsUpperCase(){
+
+        composeTestRule.onRoot(useUnmergedTree = true).printToLog("currentLabelExists")
+
+        composeTestRule
+            .onNode(
+        hasText(RallyScreen.Accounts.name.uppercase()) and
+                hasParent( hasContentDescription(RallyScreen.Accounts.name) ),
+                useUnmergedTree = true
+            )
+            .assertExists()
+    }
 
 }
